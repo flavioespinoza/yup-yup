417ea30574f5c02a5c238ee68adbe0ca6a049bb3
flavioespinoza <flavio.espinoza@gmail.com>
2019-10-10 17:57:26 -0600

    docs version

--- src/phone.js
+++ src/phone.js
@@ -1,3 +1,25 @@
+/**
+ * @name phone
+ * @description phone number input validator
+ * @example
+ *
+ * // valid: accepted phone inputs
+ * console.log(phone('(444) 444-4444')) // true
+ * console.log(phone('+1 (444) 444-4444')) // true
+ * console.log(phone('444-444-4444')) // true
+ * console.log(phone('+1 444-444-4444')) // true
+ * console.log(phone('444.444.4444')) // true
+ * console.log(phone('(444) 444 4444')) // true
+ * console.log(phone('4444444444')); // true
+ * console.log(phone('444 444 4444')) // true
+ * console.log(phone('+14444444444')) // true
+ *
+ * // invalid: too many numbers or has a letter or non-numeric character
+ * console.log(phone('44444444445')) // false
+ * console.log(phone('+144444444445')) // false
+ * console.log(phone('S444444445')) // false
+ * */
+
 const NANP_REGEXP = /^(\+?1[.\-\s]?)?\(?[2-9]\d{2}[).\-\s]?\s?[2-9]\d{2}[.\-\s]?\d{4}$/;
 
 function phone(msg) {
