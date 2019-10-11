a07a95bbb99533472ee95d33f2e944831a3a3bfa
flavioespinoza <flavio.espinoza@gmail.com>
2019-10-10 19:51:26 -0600

    date validator added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import * as yup from 'yup';
 import isRequired from './src/isRequired';
 import phone from './src/phone';
+import date from './src/date';
 
 yup.addMethod(yup.string, 'isRequired', isRequired);
 yup.addMethod(yup.number, 'isRequired', isRequired);
+yup.addMethod(yup.array, 'isRequired', isRequired);
+
 
 yup.addMethod(yup.string, 'phone', phone);
 yup.addMethod(yup.number, 'phone', phone);
+
+yup.addMethod(yup.string, 'date', date);
+yup.addMethod(yup.number, 'date', date);
